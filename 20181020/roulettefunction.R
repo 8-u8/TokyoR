3c331249019f6e9a48008b62bb8af387c8f71c70
Takafumi ITO <itotaka523@gmail.com>
2018-11-18 12:27:55 +0900

    rourettefunction.Rをsandboxに移動

--- 20181020/roulettefunction.R
+++
@@ -1,9 +0,0 @@
-LTMember <- c("Here presenters of LT as character.")
-
-roulette <- function(vec){
-  x <- length(vec)
-  vec2 <- sample(vec,x,replace = FALSE, rep(1/x,x))
-  return(vec2)
-}
-
-roulette(LTMember)
