b69a6b40aa10147f0f358cf1222696b55ff60e9c
8-u8 <itotaka523@gmail.com>
2019-01-17 23:24:43 +0900

    Source edited

--- 20190119/SessionData.R
+++ 20190119/SessionData.R
@@ -1,4 +1,38 @@
-SessionData <- data.frame(Titanic)
+Head <- function(DataFrame, n = 5){
+  DataFrame[1:n,]
+}
+Tail <- function(DataFrame, n = 5){
+  END   <- nrow(DataFrame)
+  START <- END-n
+  DataFrame[START:END,]
+}
 
-summary(SessionData)
+EXPm1 <- function(x){
+  exp(x-1)	
+}
+
+"%+=%" <- function(x,y){x + y}
+"%-=%" <- function(x,y){x - y}
+
+"%>1%" <- function(x, Function){Function(x)}
+
+HasDot <- function(expr){
+  any(as.list(expr) == substitute(.))
+}
+
+InsertDot <- function(expr){
+  as.call(c(expr[[1]], quote(.), as.list(expr[-1])))
+}
+
+"%>2%" <- function(LeftHands, RightHands){
+  env <- parent.frame()
+  expr <- substitute(RightHands)
+  dotted <- if(HasDot(expr)) expr else InsertDot(expr)
+  eval(dotted, list(. = LeftHands), env)
+}
+
+"%fillna%" <- function(x,y){
+  x[is.na(x)] <- y
+  return(x)
+}
 
